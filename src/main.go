1cf16ab2cf598eb4a52d8150d0741c12dc088b6b
Alvina Waseem <awaseem2@illinois.edu>
2020-09-15 23:50:27 -0500

    skeleton of input handling

--- src/main.go
+++ src/main.go
@@ -1,30 +1,78 @@
 package main
 
 import (
-	"fmt"
-	"os"
+		"bufio"
+		"fmt"
+		"os"
+		"strings"
 )
 
 func main() {
-	// TODO: read config files
-	// TODO: wait for input to query operations on node?
-	// TODO: join -> sendmessage to introducer to add to list, gets list in return
-	// TODO: start listening after recieving membershiplist, announce to random member or smth
+        // TODO: read config files
+        // TODO: join -> sendmessage to introducer to add to list, gets list in return
+        // TODO: start listening after recieving membershiplist, announce to random member or smth
+        
+        // Test send/recv UDP packet
+        // Start a listener somewhere with ./main listen <port>
+		// Send a text message: ./main send <ip>:<port> <message>
+		if len(os.Args) > 2 {
+			arg := os.Args[1]
 
-	// Test send/recv UDP packet
-	// Start a listener somewhere with ./main listen <port>
-	// Send a text message: ./main send <ip>:<port> <message>
-	arg := os.Args[1]
+			switch arg {
+			case "send":
+					SendMessage(os.Args[2], os.Args[3])
+			case "listen":
+					Listener(os.Args[2])
+			}
+		}
 
-	switch arg {
-	case "send":
-		SendMessage(os.Args[2], os.Args[3])
-	case "listen":
-		Listener(os.Args[2])
-	case "readConfig":
-		ReadConfig()
-	default:
-		fmt.Println("Invalid arguments.")
-	}
+		configFile := ReadConfig()
+		serviceInfo := configFile["service"]
+		service := serviceInfo.(map[string]interface{})
+
+		// wait for input to query operations on node
+		fmt.Println("Listening for input.")
+		fmt.Println("Options: join introducer, join, leave, kill, status, get logs {-a}.")
+		for {
+			consoleReader := bufio.NewReader(os.Stdin)
+			fmt.Print("> ")
+			input, _ := consoleReader.ReadString('\n')
+	
+			input = strings.ToLower(strings.TrimSuffix(input, "\n"))
+
+			switch input {
+			case "join introducer":
+				port := service["port"]
+				if str, ok := port.(string); ok {
+					Listener(str)
+				}
+				fmt.Println("Node has joined the group.")
+
+			case "join":
+				// Join()
+				fmt.Println("Node has joined the group.")
+
+			case "leave":
+				// 	Leave()
+				fmt.Println("Node has left the group.")
+				
+			case "kill":
+				// simulate a failure?
+				fmt.Println("Killing process. Bye bye.")
+				os.Exit(1)
+
+			case "status":
+				// TODO
+				fmt.Println("[imagine some status here].")
+
+			case "get logs -a":
+				// TODO
+				fmt.Println("[imagine some logs here].")
+
+			case "get logs":
+				// TODO
+				fmt.Println("[imagine some logs here].")
+			}
+		}
 
 }
