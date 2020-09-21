ba3060d6ad7ae95aa93d2920df9d5dc5c3e9f340
James Timotiwu <architect.idol5@gmail.com>
2020-09-21 10:13:36 -0400

    Some changes in main

--- src/main.go
+++ src/main.go
@@ -15,9 +15,6 @@ var (
 )
 
 func main() {
-	// TODO: wait for input to query operations on node?
-	// TODO: start listening after recieving membershiplist, announce to random member or smth
-
 	// Set up loggers and configs
 	Log(os.Stdout, os.Stdout, os.Stderr)
 	Configuration = ReadConfig()
@@ -30,80 +27,76 @@ func main() {
 		consoleReader := bufio.NewReader(os.Stdin)
 		fmt.Print("> ")
 		input, _ := consoleReader.ReadString('\n')
-
 		input = strings.ToLower(strings.TrimSuffix(input, "\n"))
+		args := strings.Fields(input) // Split string into os.Args like array
 
-		if strings.Contains(input, "join introducer") {
-			process = NewMember(true)
-			process.membershipList[0] = NewMembershipListEntry(0, net.ParseIP(Configuration.Service.introducerIP))
-			go process.Listen(fmt.Sprint(Configuration.Service.port))
-			Info.Println("You are now the introducer.")
+		if len(args) == 0 {
+			Info.Println("invalid command")
+			continue
+		}
 
-		} else if strings.Contains(input, "join") {
-			// Temporarily, the memberID is 0, will be set to correct value when introducer adds it to group
-			process = NewMember(false)
-			process.joinRequest()
-			go process.Listen(fmt.Sprint(Configuration.Service.port))
-			Info.Println("Node has joined the group.")
-
-		} else if strings.Contains(input, "leave") {
+		switch args[0] {
+		case "join":
+			if len(args) == 2 && args[1] == "introducer" {
+				process = NewMember(true)
+				process.membershipList[0] = NewMembershipListEntry(0, net.ParseIP(Configuration.Service.introducerIP))
+				go process.Listen(fmt.Sprint(Configuration.Service.port))
+				Info.Println("You are now the introducer.")
+			} else {
+				// Temporarily, the memberID is 0, will be set to correct value when introducer adds it to group
+				process = NewMember(false)
+				process.joinRequest()
+				go process.Listen(fmt.Sprint(Configuration.Service.port))
+				Info.Println("Node has joined the group.")
+			}
+		case "leave":
 			// 	Leave()
 			// TODO: Call Member.leave() here
 			Info.Println("Node has left the group.")
-
-		} else if strings.Contains(input, "kill") {
+		case "kill":
 			// simulate a failure?
 			Warn.Println("Killing process. Bye bye.")
 			os.Exit(1)
-
-		} else if strings.Contains(input, "status") {
+		case "status":
 			// TODO
 			process.PrintMembershipList(os.Stdout)
 			Info.Println("[imagine some status here].")
-
-		} else if strings.Contains(input, "get logs -a") {
-			// TODO
-			Info.Println("[imagine some logs here].")
-
-		} else if strings.Contains(input, "get logs") {
-			// TODO
-			Info.Println("[imagine some logs here].")
-
-		} else if strings.Contains(input, "chat -a") {
+		case "get":
+			if len(args) >= 2 && args[1] == "logs" {
+				if len(args) == 3 && args[2] == "-a" {
+					Info.Println("[imagine some logs logs here].")
+				} else {
+					Info.Println("[imagine some logs here].")
+				}
+			}
+		case "chat":
 			// FOR DEBUGGING PURPOSES
-
 			// DEBUG addresses
 			addresses := []string{"172.22.156.42:9000", "172.22.158.42:9000", "172.22.94.42:9000", "172.22.156.43:9000"}
 			Info.Println("Joined chat")
-
 			for {
 				consoleReader := bufio.NewReader(os.Stdin)
 				fmt.Print("> ")
 				input, _ := consoleReader.ReadString('\n')
 				SendBroadcast(addresses, 2, []byte(input))
 			}
-
-		} else if strings.Contains(input, "start") {
+		case "start":
 			if process == nil {
 				Warn.Println("You are not in a group.")
 			}
-
 			go process.Tick()
-
-		} else if strings.Contains(input, "stop") {
+		case "stop":
 			if enabledHeart == true {
 				disableHeart <- true
 			}
-
-		} else if strings.Contains(input, "switch gossip") {
-			SetHeartbeating(true)
-
-		} else if strings.Contains(input, "switch alltoall") {
-			SetHeartbeating(false)
-
-		} else {
+		case "switch":
+			if args[1] == "gossip" {
+				SetHeartbeating(true)
+			} else if args[1] == "alltoall" {
+				SetHeartbeating(false)
+			}
+		default:
 			Info.Println("invalid command")
-
 		}
 	}
 
