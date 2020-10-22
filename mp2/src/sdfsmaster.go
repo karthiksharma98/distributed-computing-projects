32270ced3c686912394aa6e1ce8fedbbab3d88a3
Kartikeya Sharma <karthiklfhs@gmail.com>
2020-10-21 19:41:54 -0500

    bugfix?

--- mp2/src/sdfsmaster.go
+++ mp2/src/sdfsmaster.go
@@ -386,6 +386,7 @@ func (node *SdfsNode) handleReplicationOnFailure(memberID uint8) error {
 
 		errorLoop:
 			for {
+				fmt.Println(newIP, chosenIP)
 				switch err := err.(type) {
 				case nil:
 					break errorLoop
@@ -393,6 +394,7 @@ func (node *SdfsNode) handleReplicationOnFailure(memberID uint8) error {
 					if err.ip.Equal(newIP) {
 						// can't connect to newIP and upload there, try another
 						failedIPList = append(failedIPList, newIP)
+						fmt.Println("failed IPS:", failedIPList)
 						newIP := findNewReplicaIP(node.Member.membershipList, filename, failedIPList, ipList)
 						if newIP == nil {
 							return errors.New("No available IP to upload to for " + filename)
@@ -400,6 +402,7 @@ func (node *SdfsNode) handleReplicationOnFailure(memberID uint8) error {
 					} else if err.ip.Equal(chosenIP) {
 						// can't connect to existing replica to upload, try another
 						failedIPList = append(failedIPList, chosenIP)
+						fmt.Println("failed IPS:", failedIPList)
 						chosenIP := chooseIP(ipList, failedIPList)
 						if chosenIP == nil {
 							return errors.New("All replicas dead for " + filename)
