4f56f748326b8d463610cacd1197842b2baab25b
James Timotiwu <architect.idol5@gmail.com>
2020-11-21 21:35:33 -0500

    Add some more boiler

--- mp3/src/test/test.go
+++ mp3/src/test/test.go
@@ -16,14 +16,6 @@ type JuiceRequest struct {
         keys []string
 }
 
-func MockFile() []byte {
-        sl := make([]byte, 8 << 20)
-        for i := 0; i < cap(sl); i++ {
-                sl[i] = 'B'
-        }
-        return sl
-}
-
 // Pull data and shuffle/sort into a single value
 func ShuffleSort(prefix string, key string) string {
         // Get ips of file
@@ -55,8 +47,8 @@ func ShuffleSort(prefix string, key string) string {
 
 func RpcJuice(req JuiceRequest, reply *JuiceReply) {
         keys = []string{"placeholder1", "placeholder2"}
-        pref = "placeholder_prefix"
-        exeName = "placeholder_exe"
+        pref = req.IntermediatePrefix
+        exeName = "./" + req.ExeName
         for _, key := range keys {
                 // Run shuffler
                 sortedFruits := ShuffleSort(key, pref)
@@ -66,19 +58,20 @@ func RpcJuice(req JuiceRequest, reply *JuiceReply) {
         return
 }
 
-func Juice(mapleQueueReq MapleJuiceQueueRequest) {
+func Juice(juiceQueueReq MapleJuiceQueueRequest) {
         // run partitioner at juice request
         keys := []string{"key1", "key2"}
         numJuices := 5
         partitions := partitioner(keys, numJuices, false)
+        outputFname := juiceQueueReq.FileList[0]
 
         // RequestJuiceTask
         for id, list := range partitions {
                 // Get ip list, choose node with key id % numNodes
                 var req JuiceRequest
-                req.ExeName = mapleQueueReq.ExeName
-                req.IntermediatePrefix = mapleQueueReq.IntermediatePrefix
-                req.fileName = "placeholder"
+                req.ExeName = juiceQueueReq.ExeName
+                req.IntermediatePrefix = juiceQueueReq.IntermediatePrefix
+                req.fileName = outputFname
                 req.key = list
                 // Send Juice Request to that partition, with key to reduce
                 // Get ip address of id
@@ -90,25 +83,51 @@ func Juice(mapleQueueReq MapleJuiceQueueRequest) {
         // TODO: Download juice output from corresponding SDFS file (JuiceCollector)
         // TODO: Open juice output files
         // TODO: Join and append together
-        CollectJuices(partitions)
+        filePath := path.Join([]string{mapleJuiceDirName, outputFname}...)
+        CollectJuices(filePath, juiceQueueReq.FileList[0]partitions)
         // TODO: Upload file to SDFS
+        if sdfs == nil {
+                fmt.Println("Unable to upload juice output to SDFS.")
+        }
+        sessionId := sdfs.RpcLock(int32(sdfs.Member.memberID), outputFname, SdfsLock)
+        sdfs.RpcPut(filePath, outputFname)
+        _ := sdfs.RpcUnlock(sessionId, outputFname, SdfsLock)
         return
 }
 
-func CollectJuices(partitions map[int][]string) {
+func CollectJuices(outFname string, partitions map[int][]string) {
         allJuices := ""
         for id, list := range partitions {
                 // TODO: Get ip address of k
                 ipAddr := "10.0.0.1"
                 for _, key := range list {
+                        filePath := path.Join([]string{mapleJuiceDirName, prefix + "_" + key}...)
                         _ := Download(ipAddr, fmt.Sprint(Configuration.Service.filePort), mapleJuiceDirName, prefix + "_" + key)
                         // TODO: Open file
+                        file, err := os.Open(filePath)
+                        if err != nil {
+                                panic(err)
+                        }
+                        defer file.Close()
+
                         // Append to allJuices file
-                        buf = "someKey 100"
-                        allJuices = allJuices + buf + "\n"
+                        s := bufio.NewScanner(file)
+                        for s.Scan() {
+                                allJuices = allJuices + s.Text() + "\n"
+                        }
                 }
         }
         // TODO: Save all juices to local folder of collected juice
+        fileFlags := os.O_CREATE | os.O_WRONLY
+        file, err := os.OpenFile(filePath, fileFlags, 0777)
+        if err != nil {
+                fmt.Println(err)
+        }
+        defer file.Close()
+
+        if _, err := file.WriteString(allJuices); err != nil {
+                fmt.Println(err)
+        }
 }
 
 func RequestJuiceTask(chosenIp string, req JuiceRequest) {
@@ -120,7 +139,7 @@ func RequestJuiceTask(chosenIp string, req JuiceRequest) {
         }
 
         var res MapleJuiceReply
-        err = mapleClient.Call("SdfsNOde.RpcJuice", req, &res)
+        err = mapleClient.Call("SdfsNode.RpcJuice", req, &res)
         if err != nil || !res.Completed {
                 fmt.Println(err)
         }
