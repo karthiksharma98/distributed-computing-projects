0d7a4cc6c13ae0b8ee2a8df9d1f3ad276dab8245
awaseem2 <awaseem2@illinois.edu>
2020-11-21 22:41:08 -0600

    changed maple to read files from src directory

--- mp3/src/sdfsmaster.go
+++ mp3/src/sdfsmaster.go
@@ -15,10 +15,11 @@ import (
 )
 
 type SdfsMaster struct {
-	fileMap   map[string]map[int][]net.IP
-	numBlocks map[string]int
-	lockMap   map[string]*SdfsMutex
-	sessMap   map[int32](chan bool)
+	fileMap      map[string]map[int][]net.IP
+	sdfsFNameMap map[string]string // localName -> sdfsName
+	numBlocks    map[string]int
+	lockMap      map[string]*SdfsMutex
+	sessMap      map[int32](chan bool)
 }
 
 type connectionError struct {
@@ -40,6 +41,7 @@ var (
 func NewSdfsMaster() *SdfsMaster {
 	master := &SdfsMaster{
 		make(map[string]map[int][]net.IP),
+		make(map[string]string),
 		make(map[string]int),
 		make(map[string]*SdfsMutex),
 		make(map[int32](chan bool)),
