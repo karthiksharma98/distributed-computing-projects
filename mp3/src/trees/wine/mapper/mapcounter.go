dd75ee3f240a03f14a52d9accc8dec58150c363d
Timotiwu <jit2@fa20-cs425-g13-01.cs.illinois.edu>
2020-11-29 22:39:03 -0600

    Winery

--- mp3/src/trees/wine/mapper/mapcounter.go
+++ mp3/src/trees/wine/mapper/mapcounter.go
@@ -2,19 +2,23 @@ package main
 
 import (
         "strings"
-	"fmt"
+	"encoding/csv"
 )
 
 func Map(key string, value string) {
-	s := strings.Split(value, ",")
-	fmt.Println(s[9])
-	if "Chardonnay" == strings.TrimSpace(s[9]) {
-		fmt.Println(s)
+	//s := strings.Split(value, ",")
+	//fmt.Println(s[9])
+	r := csv.NewReader(strings.NewReader(value))
+	s, err := r.Read()
+	if err != nil {
+		return
+	}
+
+	if "Chardonnay" == s[9] {
 		for _, word := range strings.Fields(s[2]) {
 			if word == "succulent" {
 				Emit(s[8], "1")
 			}
 		}
 	}
-	
 }
