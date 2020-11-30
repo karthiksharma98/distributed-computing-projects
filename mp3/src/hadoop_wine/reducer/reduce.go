1aa4639becb66a77813a8ef146703779d8aa49d5
Timotiwu <jit2@fa20-cs425-g13-01.cs.illinois.edu>
2020-11-29 23:42:33 -0600

    Reorganize folders

---
+++ mp3/src/hadoop_wine/reducer/reduce.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+        "fmt"
+        "bufio"
+        "strings"
+        "os"
+)
+
+var (
+        keyValues map[string][]string = make(map[string][]string)
+)
+
+func CollectReduce() {
+        // MapReduce feeds data line by line
+        s := bufio.NewScanner(os.Stdin)
+        for s.Scan() {
+                // Split strings by tab
+                line := strings.Fields(s.Text())
+                if len(line) == 2 {
+                        if val, ok := keyValues[line[0]]; ok {
+                                keyValues[line[0]] = append(val, line[1])
+                                continue
+                        }
+                        keyValues[line[0]] = []string{line[1]}
+                }
+        }
+
+        if err := s.Err(); err != nil {
+                fmt.Println(err)
+        }
+}
+
+func RunReduce() {
+        CollectReduce()
+        for key, values := range keyValues {
+                Reduce(key, values)
+        }
+}
+
+func Emit(key string, value string) {
+        fmt.Printf("%s\t%s\n", key, value)
+}
+
+func main() {
+        RunReduce()
+}
